706404270e8c9c04ca17242fc745e45ff9269041
sawada0813 <Kyoya.Sawada@jp.ricoh.com>
2024-07-30 20:11:31 +0900

    delete: 不要なファイル

--- exercises/ch10/ex03_/classes/animal.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = class Animal {
-  eat() {
-    console.log("I am eating");
-  }
-  makeSound() {
-    console.log("I am making sound");
-  }
-};
--- exercises/ch10/ex03_/utils.cjs
+++
@@ -1,4 +0,0 @@
-const Animal = require("./classes/animal.cjs");
-exports.isAnimal = (obj) => {
-  return obj instanceof Animal;
-};
