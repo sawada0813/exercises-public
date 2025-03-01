264914a33a52f259a79228e25338fba93d06f9b6
sawada0813 <Kyoya.Sawada@jp.ricoh.com>
2025-03-01 18:06:13 +0900

    refactor: enhance Pad component with keyboard interaction and support for WAV files

--- exercises/kadai/daw/app/pad.tsx
+++ exercises/kadai/daw/app/pad.tsx
@@ -1,26 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 export default function Pad(props: { id: number }) {
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const [color, setColor] = useState("bg-blue-500");
 
-  const playAudio = (audioFile: string) => {
-    const audio = new Audio(audioFile);
-    audio.play();
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.type === "audio/x-m4a") {
-      const fileUrl = URL.createObjectURL(file);
-      setAudioUrl(fileUrl);
-    } else {
-      alert("m4aファイルを選択してください");
-    }
-  };
-
-  const onClick = () => {
+  const onClick = useCallback(() => {
     if (audioUrl === null) {
       alert("音声ファイルを選択してください");
     } else {
@@ -30,6 +15,32 @@ export default function Pad(props: { id: number }) {
         setColor("bg-blue-500");
       }, 100);
     }
+  }, [audioUrl]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === props.id.toString()) {
+        onClick();
+      }
+    };
+    if (audioUrl) {
+      window.addEventListener("keydown", handleKeyDown);
+    }
+  }, [audioUrl, props.id, onClick]);
+
+  const playAudio = (audioFile: string) => {
+    const audio = new Audio(audioFile);
+    audio.play();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if ((file && file.type === "audio/x-m4a") || file.type === "audio/wav") {
+      const fileUrl = URL.createObjectURL(file);
+      setAudioUrl(fileUrl);
+    } else {
+      alert("m4aファイルを選択してください");
+    }
   };
 
   return (
@@ -37,7 +48,11 @@ export default function Pad(props: { id: number }) {
       <div className={`${color} p-4`} onClick={onClick}>
         Box {props.id}
       </div>
-      <input type='file' accept='audio/m4a' onChange={handleFileChange} />
+      <input
+        type='file'
+        accept='audio/m4a, audio/wav'
+        onChange={handleFileChange}
+      />
     </>
   );
 }
--- exercises/kadai/daw/app/soundPads.tsx
+++ exercises/kadai/daw/app/soundPads.tsx
@@ -5,7 +5,6 @@ import Pad from "./pad";
 export default function SoundPads() {
   return (
     <div className='grid grid-cols-5 grid-rows-2 gap-4'>
-      <Pad id={0} />
       <Pad id={1} />
       <Pad id={2} />
       <Pad id={3} />
@@ -15,6 +14,7 @@ export default function SoundPads() {
       <Pad id={7} />
       <Pad id={8} />
       <Pad id={9} />
+      <Pad id={0} />
     </div>
   );
 }
