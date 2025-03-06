adf97f5cdb969f37085158bc7efff8f279ba2b56
sawada0813 <Kyoya.Sawada@jp.ricoh.com>
2025-03-07 00:12:49 +0900

    refactor: remove unused import from Pad component

--- exercises/kadai/daw/app/pad.tsx
+++ exercises/kadai/daw/app/pad.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useCallback, useRef, use } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 type PadProps = {
   id: number;
