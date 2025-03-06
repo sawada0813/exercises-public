ccd400450d882e65fb4ae0e1b7391011180606f1
sawada0813 <Kyoya.Sawada@jp.ricoh.com>
2025-03-07 03:48:05 +0900

    feat: initialize Next.js project with Tailwind CSS and ESLint configuration

--- exercises/kadai/alarm/app/page.tsx
+++ exercises/kadai/alarm/app/page.tsx
@@ -135,7 +135,6 @@ export default function Home() {
         className='px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600'
         variant='outlined'
         onClick={() => {
-          // const [hour, minute, second] = alarmTime.split(":").map(Number);
           const alarm = new Date();
           alarm.setHours(Number(hours));
           alarm.setMinutes(Number(minutes));
